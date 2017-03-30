c0ba17e1a09272c9a6ab7af0d74321e935da654d
Henrik Sørensen <hhs19942@gmail.com>
2017-03-30 13:10:16 +0200

    Add simple suppot for title-text. Add keyboard navigation

--- src/inject/comic.js
+++ src/inject/comic.js
@@ -1,25 +1,26 @@
-console.log("Hej!");
 jQuery(document).ready(function() {
+	//Greeting message
+	console.log("xkcd-ext: Comic-module succesfully loaded!");
+
 	//Put title text below image
 	jQuery('#comic').after('<p>' + jQuery('#comic').children()[0].title + '<p>')
 
 	//Enable left, and right key navigation
 	jQuery(document).keydown(function(e) {
-		var elNum;
+		var elNum = 0;
 
 		switch (e.which) {
 			case 39: //Right arrow
 			case 78: //N
 			case 76: //L
 				elNum = 3;
+				break;
 			case 37: //Left arrow
 			case 80: //P
 			case 74: //J
 				elNum = 1;
-			default:
-				elNum = 0;
+				break;
 		}
-
 
 		if (elNum)
 			jQuery('.comicNav')[0].children[elNum].children[0].click();
