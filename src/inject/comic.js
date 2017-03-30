643da0930864cef13497fc958a5cd5f038afa545
Henrik Sørensen <hhs19942@gmail.com>
2017-03-30 12:59:51 +0200

    Initial commit

---
+++ src/inject/comic.js
@@ -0,0 +1,27 @@
+console.log("Hej!");
+jQuery(document).ready(function() {
+	//Put title text below image
+	jQuery('#comic').after('<p>' + jQuery('#comic').children()[0].title + '<p>')
+
+	//Enable left, and right key navigation
+	jQuery(document).keydown(function(e) {
+		var elNum;
+
+		switch (e.which) {
+			case 39: //Right arrow
+			case 78: //N
+			case 76: //L
+				elNum = 3;
+			case 37: //Left arrow
+			case 80: //P
+			case 74: //J
+				elNum = 1;
+			default:
+				elNum = 0;
+		}
+
+
+		if (elNum)
+			jQuery('.comicNav')[0].children[elNum].children[0].click();
+	});
+});
